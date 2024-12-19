6176a87d831ee37ccdfb485b970f9022e3d881d6
angelor13 <angelorrodrigues99@gmail.com>
2024-12-19 17:35:01 +0000

    fdsdgf

--- xArm-Python-SDK-master/example/wrapper/common/Final_Project.py
+++ xArm-Python-SDK-master/example/wrapper/common/Final_Project.py
@@ -81,16 +81,12 @@ iterationTime = float(0.1)
 # -> Inicialização das Constantes das Equações: 
 C = ["Cx", "Cy", "Cz"] 
 r = "r"  
-r_a = "r_a"  
+r_a = "r_a" 
 r_b = "r_b" 
 
 #Lg = 61.5
 
-<<<<<<< HEAD
-Kp = 0.9    # 0.7
-=======
-Kp = 0.9 # 0.7 -> Circunferência
->>>>>>> 9888557035ce695b81cf32d51c04ddf463da2c15
+Kp = 1 # 0.7 -> Circunferência
 Ki = 0.05   # 0.055 || 0.05
 
 # ============ Setup do Robô ============
